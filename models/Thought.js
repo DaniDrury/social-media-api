6767b025b72bce4f912ccbf56d268ac8e9852067
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-18 16:13:02 -0700

    created models/Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      immutable: true,
+      default: () => moment(),
+      get: formatDate,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ reactionSchema ],
+  },
+  {
+    virtuals: {
+      reactionCount: {
+        get() {
+          return this.reactions.length;
+        },
+      },
+    },
+    toJSON: { virtuals: true, getters: true },
+  },
+);
+
+function formatDate(createdAt) {
+  return createdAt.format("MM, DD, YYYY");
+};
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
