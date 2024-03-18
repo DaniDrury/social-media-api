ebfda77b540801e10387a9ff49e905e3d1aadb9b
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-18 16:13:22 -0700

    created models/User.js

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]{2,4}$/.test(value);
+        },
+        message: 'Invalid email address format',
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    virtuals: {
+      friendCount: {
+        get() {
+          return this.friends.length;
+        }
+      },
+    },
+  },
+  {
+    toJSON: { virtuals: true }
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
